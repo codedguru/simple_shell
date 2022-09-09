263cb45892887360b63200a2f49e8b480a3290b1
Mali0131 <106757530+Mali0131@users.noreply.github.com>
2022-09-09 08:13:35 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 {
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
