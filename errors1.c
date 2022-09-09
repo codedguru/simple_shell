d4ef5c890af6670c29401b820111bcf02b6c297e
Mali0131 <106757530+Mali0131@users.noreply.github.com>
2022-09-09 04:33:09 -0700

    Update errors1.c

--- errors1.c
+++ errors1.c
@@ -1,4 +1,11 @@
+#include "shell.h"
 
+/**
+ * _erratoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ *       -1 on error
+ */
 int _erratoi(char *s)
 {
 	int i = 0;
