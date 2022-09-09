561b36e322643637caf9316126729ab605d653b3
Mali0131 <106757530+Mali0131@users.noreply.github.com>
2022-09-09 08:00:03 -0700

    Update getinfo.c

--- getinfo.c
+++ getinfo.c
@@ -1,3 +1,23 @@
+#include "shell.h"
+
+/**
+ * clear_info - initializes info_t struct
+ * @info: struct address
+ */
+void clear_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
+
+/**
+ * set_info - initializes info_t struct
+ * @info: struct address
+ * @av: argument vector
+ */
+void set_info(info_t *info, char **av)
 {
 	int i = 0;
 
